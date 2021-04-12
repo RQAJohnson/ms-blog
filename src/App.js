641db386d0a7ad01fe9511743639cefadfed6250
RQAJohnson <RQAJohnson@qa.com>
2021-04-12 16:31:51 +0100

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import{BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Contact from './Components/Contact/Contact';
+import Symptoms from './Components/Symptoms/Symptoms'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style={{
+      backgroundColor: 'white'
+    }}>
+     {/* <p>Hello world!</p> */}
+
+     <Router>
+     <Navigation/>
+     <Switch>
+     <Route path="/" exact>
+      <Home/>
+     </Route>
+     <Route path="/about">
+      <About/>
+     </Route>
+     <Route path="/contact">
+      <Contact/>
+     </Route>
+     <Route path="/symptoms">
+      <Symptoms/>
+     </Route>
+     </Switch>
+     {/* <Footer/> */}
+     </Router>
+     
     </div>
+    
   );
 }
+
 
 export default App;
