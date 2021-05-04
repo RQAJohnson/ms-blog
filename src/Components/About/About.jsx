ef8aa8c9d16ef9c84b1dcc52de22b0110f98a2fb
RQAJohnson <RQAJohnson@qa.com>
2021-05-04 18:49:51 +0100

    swapped the paragraph under the developer tag for the one underneath the who am i

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,8 +9,7 @@ const About = (props) => {
     <h1 className="text-black text-center display-4">Who am I?</h1>
 
     <div className="container" align="center" >
-    <p className="lead text-center text-black pt-2 mb-5">A big dreamer who loves all things creative from learning about AI to watching shows 
-    like 'Black Mirror' and 'Alice in Borderland'.</p>
+    <p className="lead text-center text-black pt-2 mb-5">No I'm not drunk, I have MS. (<i>also, I'm a little drunk</i>) -anon.</p>
         
         <div class="col-md-3 mb-4">
             <div class="view z-depth-1 mb-4">
@@ -19,12 +18,12 @@ const About = (props) => {
                 </div>
                 <h4 className="text-black">Rianna Johnson</h4>
                 <medium class="text-black">Developer</medium>
-                <p class="mt-3 text-black">No, I'm not drunk. I have MS. -anon</p>
+                <p class="mt-3 text-black">A big dreamer who loves all things creative from learning about AI to watching shows like 'Black Mirror' and 'Alice in Borderland'.</p>
                 <ul class="list-unstyled d-flex justify-content-center mt-3 mb-0 text-muted">
                     <li><a class="nav-link" href="https://github.com/RQAJohnson" target="_blank">
-                        <Github className="mx-3"color="black" size={40}/></a></li>
+                        <Github className="mx-3"color="black" size={35}/></a></li>
                         <li><a class="nav-link" href="https://www.linkedin.com/in/riannasj" target="_blank">
-                        <Linkedin className="mx-3"color="black" size={40}/></a></li>
+                        <Linkedin className="mx-3"color="black" size={35}/></a></li>
                 </ul>
         </div>
         </div>
