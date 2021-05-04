92cda75dedd4f07d3f50483496540944eaee8312
RQAJohnson <RQAJohnson@qa.com>
2021-05-04 18:00:46 +0100

    imagees and header have been added to the Abuot page

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,24 +1,32 @@
-import { Linkedin } from "react-bootstrap-icons";
+import { Linkedin, Github} from "react-bootstrap-icons";
+
 
 const About = (props) => {
     return (
-        <>
+    <>
+    <br></br>
+    <br></br>
+    <h1 className="text-black text-center display-4">Who am I?</h1>
+
+    <div className="container" align="center" >
+    <p className="lead text-center text-black pt-2 mb-5">vghguohfgbaifb</p>
+        
         <div class="col-md-3 mb-4">
             <div class="view z-depth-1 mb-4">
-                <img src="https://images.unsplash.com/photo-1526498460520-4c246339dccb?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzd8fHByb2dyYW1taW5nfGVufDB8MHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" 
-                class="img-fluid mx-auto" alt="smaple image"/>
+                <img src="https://images.unsplash.com/photo-1590194360975-c250dde6520d?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDExNHxfaGItZGw0US00VXx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" 
+                class="z-depth-1 rounded-circle img-fluid" alt="smaple image"/>
                 </div>
-                <h6 class="font-weight-bold text-white">Rianna Johnson</h6>
-                <small class="text-white">Developer</small>
-                <p class="mt-3 text-white">Tom Hanks's brother (Jim Hanks) voiced Sheriff Woody in Toy Story when he was too busy.</p>
+                <h4 className="text-black">Rianna Johnson</h4>
+                <medium class="text-black">Developer</medium>
+                <p class="mt-3 text-black">Tom Hanks's brother (Jim Hanks) voiced Sheriff Woody in Toy Story when he was too busy.</p>
                 <ul class="list-unstyled d-flex justify-content-center mt-3 mb-0 text-muted">
                     <li><a class="nav-link" href="https://github.com/RQAJohnson" target="_blank">
-                        <Github className="mx-3"color="white" size={40}/></a></li>
-                        <li><a class="nav-link" href="www.linkedin.com/in/rianna-johnson-19307b132" target="_blank">
-                        <Linkedin className="mx-3"color="white" size={40}/></a></li>
+                        <Github className="mx-3"color="black" size={40}/></a></li>
+                        <li><a class="nav-link" href="https://www.linkedin.com/in/riannasj" target="_blank">
+                        <Linkedin className="mx-3"color="black" size={40}/></a></li>
                 </ul>
         </div>
-        <h1>my name is rianna</h1>
+        </div>
         </>
     );
 }
