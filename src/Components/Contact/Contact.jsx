01211a1db214e5868409fe416c1236a53cc5c3a8
RQAJohnson <RQAJohnson@qa.com>
2021-05-04 19:30:57 +0100

    made a note of what I am looking to add to the contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,11 @@
 
 const Contact = (props) => {
     return (
-        <h1>my email address is...</h1>
+        <>
+        <h1>add email infomation here</h1>
+        <p>Add information about who to contact on the external 
+            site (NHS Website) and then use website emojicon</p>
+        </>
     );
 }
 
