89c3d8d4279c64eec001a98d25f6cad804e4795b
RQAJohnson <RQAJohnson@qa.com>
2021-05-05 18:17:42 +0100

    added a para with subheadings so users know what to expect from the page

--- src/Components/Symptoms/Symptoms.jsx
+++ src/Components/Symptoms/Symptoms.jsx
@@ -1,14 +1,15 @@
 
 const Symptoms = (props) => {
     return (
-        <>
-        <br></br>
-        <br></br>
-        <h1 className="text-black text-center display-4">My Leg Hurts...</h1>
-        <p>What do I need to look out for?</p>
-        <p>
-        
+    <>
+    <br></br>
+    <br></br>
+    <h1 className="text-black text-center display-4">My Leg Hurts...</h1>
+    <div className="container" align="center">
+        <p className="lead text-center text-black pt-2 mb-5"> What this means for me? | Diagnosis | How can it be treated?
         </p>
+    </div>
+    
         </>
     );
 }
