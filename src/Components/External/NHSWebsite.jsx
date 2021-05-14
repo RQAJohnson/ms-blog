4d51ad673ec729f2fceeb242de1105be6d114b03
RQAJohnson <RQAJohnson@qa.com>
2021-05-14 15:54:35 +0100

    information about the NHS webpage has been provided

--- src/Components/External/NHSWebsite.jsx
+++ src/Components/External/NHSWebsite.jsx
@@ -1,9 +1,32 @@
+import {Link} from 'react-router-dom';
 
 const Website = (props) => {
     return (
         <>
         <br></br>
-        <h1 className="text-black text-center display-4">MS Trust</h1>
+        <h1 className="text-black text-center display-4">NHS Website</h1>
+        <br></br>
+        <br></br>
+
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img width="100%"src='NHSWebsite2.png' alt="Cup of Pills"/>
+                    </div>
+                    
+                    <div class="col">
+                        <p>Providing concise and easy to read information, the NHS has been able to condense vital information
+                            someone may need in order to seek out and learn about Multiple Sclerosis. The informations has been 
+                            laid out across the site into subheading categories and means that if there is a specifc section 'symptoms'
+                            that you are looking to read; learn or get advice about, you are able to navigate directly to the section 
+                            yuo require. 
+                        </p>
+                        <br></br>
+                        <p>Fancy a look around checkout - <br></br>
+                            <Link to={{ pathname:"https://www.nhs.uk/conditions/multiple-sclerosis/"}} target="MS Society">NHS</Link></p>
+                        </div>
+                        </div>
+                        </div>
         </>
     );
 }
