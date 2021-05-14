b333f92ca96e27d5d5ce80466c803ce51f9f1c3d
RQAJohnson <RQAJohnson@qa.com>
2021-05-14 15:29:56 +0100

    Trust page has been completed, and line breaks have been added to the other external pages

--- src/Components/External/MSTrust.jsx
+++ src/Components/External/MSTrust.jsx
@@ -1,7 +1,27 @@
 
 const MSTrust = (props) => {
     return (
-        <h1>MS Trust site will go here</h1>
+        <>
+        <br></br>
+        <h1 className="text-black text-center display-4">MS Trust</h1>
+       
+
+        <div className="container" align="center">
+        <img src='MSTrustFINAL.png' alt="" width="40%"/>
+        <p>This site alongisde MS Society, is one of the 2 non profit organisations that seek to raise awareness
+            about MS, by providing information about sponsored runs to raise money for research purposes; advice about the 
+            latest health requirements that may play a significant role in the affects MS could play someones life (COVID).
+            From the moment of diagnosis to the day to day activities that may be negatively affected by the disease, the organisation
+            is there to support a person in any way they can.
+        </p>
+        <br></br>
+        <p>- Funding specialist MS nurses</p>
+        <p>- Providing trusted information</p>
+        <p>- Answering questions</p>
+        <br></br>
+
+</div>
+        </>
     );
 }
 
